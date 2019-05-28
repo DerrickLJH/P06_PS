ad7142436f9316347fe601627feb9dacade59f76
17010144 <17010144@myrp.edu.sg>
2019-05-28 13:28:50 +0800

    commit 1

--- app/src/main/java/com/myapplicationdev/android/p06_ps/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p06_ps/DBHelper.java
@@ -1,4 +1,80 @@
 package com.myapplicationdev.android.p06_ps;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_TASK= "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESC = "description";
+    private static final String COLUMN_TIME = "time";
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_TASK + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME +  "TEXT,"
+                + COLUMN_DESC + " TEXT, "
+                 + COLUMN_TIME +  "INTEGER)";
+        db.execSQL(createNoteTableSql);
+
+
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("ALTER TABLE " + TABLE_TASK + " ADD COLUMN module_name TEXT");
+
+    }
+
+    public long insertNote(String name , String Desc) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESC, Desc);
+        long result = db.insert(TABLE_TASK, null, values);
+        db.close();
+        Log.d("SQL Insert ",""+ result); //id returned, shouldn’t be -1
+        return result;
+
+    }
+    public ArrayList<Task> getAllTask() {
+        //TODO return records in Java objects
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_NAME + ", "
+                + COLUMN_DESC + ", "
+                + COLUMN_TIME
+                + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String desc = cursor.getString(2);
+                int time = cursor.getInt(3);
+
+                Task obj = new Task(id, name, desc, time);
+                tasks.add(obj);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
 }
--- app/src/main/java/com/myapplicationdev/android/p06_ps/Task.java
+++ app/src/main/java/com/myapplicationdev/android/p06_ps/Task.java
@@ -1,4 +1,33 @@
 package com.myapplicationdev.android.p06_ps;
 
-public class Task {
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private int id;
+    private String name;
+    private String desc;
+    private int time;
+
+    public Task(int id, String name, String desc, int time) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
