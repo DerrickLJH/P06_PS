7547e37b4fed8584868439ea9d0138cb9b32cf10
17010489 <elainenkx@gmail.com>
2019-05-28 13:30:14 +0800

    MainActivity, Task, TaskAdapter commit

--- app/src/main/java/com/myapplicationdev/android/p06_ps/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p06_ps/MainActivity.java
@@ -2,12 +2,31 @@ package com.myapplicationdev.android.p06_ps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvTasks;
+    ArrayAdapter aa;
+    ArrayList<Task> task;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvTasks = this.findViewById(R.id.lvTasks);
+
+        task = new ArrayList<Task>();
+        task.add(new Task("1 Buy Milk", "Low Fat"));
+
+
+        aa = new TaskAdapter(this, R.layout.row, task);
+        lvTasks.setAdapter(aa);
     }
 }
--- app/src/main/java/com/myapplicationdev/android/p06_ps/Task.java
+++ app/src/main/java/com/myapplicationdev/android/p06_ps/Task.java
@@ -1,4 +1,18 @@
 package com.myapplicationdev.android.p06_ps;
 
 public class Task {
+    private String name;
+    private String description;
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
